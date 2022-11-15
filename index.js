acaa2498d40550d6963c206f540b32f96a8b11db
MarioWork <mariovieira.work@hotmail.com>
2022-11-15 21:21:23 -0100

    generate and return a jwt

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = required('express');
+require('dotenv').config()
+const express = require('express');
+const jwt = require('jsonwebtoken');
 const server = express();
+
+server.use(express.json())
 
 const posts = [
     {
@@ -13,7 +17,19 @@ const posts = [
 ]
 
 server.get('/posts', (req, res) => {
+    res.json(posts);
+});
 
+server.post('/login', (req, res) => {
+    //Authenticate User
+
+    //Serialize user
+    const username = req.body.username;
+
+    const user = { name: username };
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken });
 });
 
 
