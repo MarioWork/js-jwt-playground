9e812cec736c38afd1e44e36e13e32de66c88260
MarioWork <mariovieira.work@hotmail.com>
2022-11-26 21:21:49 -0100

    easier export

--- utils/generate-access-token.js
+++ utils/generate-access-token.js
@@ -3,4 +3,4 @@ const jwt = require('jsonwebtoken');
 const generateAccessToken = (user) =>
     jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30s' });
 
-module.exports = { generateAccessToken }
+module.exports = generateAccessToken;
--- utils/generate-refresh-access-token.js
+++ utils/generate-refresh-access-token.js
@@ -2,4 +2,4 @@ const jwt = require('jsonwebtoken');
 
 const generateRefreshAccessToken = (user) => jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
 
-module.exports = { generateRefreshAccessToken };
+module.exports = generateRefreshAccessToken;
