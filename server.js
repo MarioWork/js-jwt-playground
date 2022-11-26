60f48c30d62fcaea04a88c90e296bff336c25610
MarioWork <mariovieira.work@hotmail.com>
2022-11-26 21:22:55 -0100

    code refactor

--- server.js
+++ server.js
@@ -1,34 +1,11 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
 
 
-const posts = [
-    {
-        username: 'Kyle',
-        title: 'Title 1'
-    },
-    {
-        username: 'John',
-        title: 'Title 2'
-    },
-]
-
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
 
 server.use('/login', require('./routes/login'));
 
