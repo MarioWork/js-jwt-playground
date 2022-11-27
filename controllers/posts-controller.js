0d4dcaaf86f965dbe6bf3449e14cfaf621e7981d
MarioWork <mariovieira.work@hotmail.com>
2022-11-27 00:12:06 -0100

    create a posts controller

---
+++ controllers/posts-controller.js
@@ -0,0 +1,8 @@
+
+const posts = require('../data/posts');
+
+const getPostsByUsername = (req, res) => {
+    res.json(posts.filter(({ username }) => username === req.user.name))
+};
+
+module.exports = { getPostsByUsername };
