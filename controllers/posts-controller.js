631cf857bc9e9e6912293f0d3c6923645838ff74
MarioWork <mariovieira.work@hotmail.com>
2022-11-27 20:54:50 -0100

    add optional chaining

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,8 +1,7 @@
 
 const posts = require('../data/posts');
-
 const getPostsByUsername = (req, res) => {
-    res.json(posts.filter(({ username }) => username === req.user.name))
+    res.json(posts?.filter(({ username }) => username === req.user.name))
 };
 
 module.exports = { getPostsByUsername };
