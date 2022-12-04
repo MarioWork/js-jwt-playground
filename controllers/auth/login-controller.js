11c683e98ccc36891061581a3e92a1acee706f36
MarioWork <mariovieira.work@hotmail.com>
2022-12-04 17:49:23 -0100

    save refresh tokens on local file

--- controllers/auth/login-controller.js
+++ controllers/auth/login-controller.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { refreshTokens, addToken } = require('../../data/refresh-tokens');
+const { saveRefreshToken } = require('../../services/local-storage-tokens-service');
 
 const generateAccessToken = require('../../utils/generate-access-token');
 const generateRefreshAccessToken = require('../../utils/generate-refresh-access-token');
@@ -16,10 +16,9 @@ const handleLogin = (req, res) => {
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshAccessToken(user);
 
-    addToken(refreshToken);
+    saveRefreshToken(refreshToken);
 
     res.json({ accessToken, refreshToken });
-    console.log(refreshTokens);
 }
 
 module.exports = { handleLogin };
