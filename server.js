2646e3f0468b8b7cd41a481825a88a2899172164
MarioWork <mariovieira.work@hotmail.com>
2022-12-05 23:33:13 -0100

    use cookie parse middleware

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const server = express();
 
+//Middleware
 server.use(express.json());
+server.use(cookieParser());
 
 server.use('/login', require('./routes/auth/login'));
 server.use('/refresh', require('./routes/auth/refresh'));
